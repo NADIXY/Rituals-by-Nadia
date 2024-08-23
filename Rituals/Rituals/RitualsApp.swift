24464db9d895902ba5d923d7bf60da5a939c9916
NADIXY <nadiagbaptista015@gmail.com>
2024-08-23 15:11:27 +0200

    Start

---
+++ Rituals/Rituals/RitualsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RitualsApp.swift
+//  Rituals
+//
+//  Created by Nadia Marina Gaspar Baptista on 23.08.24.
+//
+
+import SwiftUI
+
+@main
+struct RitualsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
