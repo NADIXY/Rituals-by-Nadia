2704480694a882a33f80c8ee79639d8709823fc7
NADIXY <nadiagbaptista015@gmail.com>
2024-08-29 15:59:40 +0200

    Model Rituals

---
+++ Rituals/Rituals/Model/Rituals.swift
@@ -0,0 +1,20 @@
+//
+//  Rituals.swift
+//  Rituals
+//
+//  Created by Nadia on 29.08.24.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Rituals: Identifiable, Codable {
+    @DocumentID var id: String?
+    var name: String
+    var description: String
+    var image: String
+    var location: String
+    var isFavorite: Bool
+    
+    
+}
--- Rituals/Rituals/RitualsApp.swift
+++ Rituals/Rituals/RitualsApp.swift
@@ -7,17 +7,21 @@
 
 import SwiftUI
 import Firebase
-//import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct RitualsApp: App {
-    @StateObject var viewModel = AuthViewModel()
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    init() {
-        FirebaseConfiguration.shared.setLoggerLevel(.min)
-        FirebaseApp.configure()
-        print("Firebase wurde konfiguriert")
-    }
+    @StateObject var viewModel = AuthViewModel()
     
     var body: some Scene {
         WindowGroup {
@@ -26,3 +30,4 @@ struct RitualsApp: App {
         }
     }
 }
+
