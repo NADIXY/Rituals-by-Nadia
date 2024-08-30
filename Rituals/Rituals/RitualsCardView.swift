b05b27e1f6b6c49dc9776c6223fdeb0bb07dd2e3
NADIXY <nadiagbaptista015@gmail.com>
2024-08-30 08:37:55 +0200

    Rituals Models and Views

---
+++ Rituals/Rituals/RitualsCardView.swift
@@ -0,0 +1,18 @@
+//
+//  RitualsCardView.swift
+//  Rituals
+//
+//  Created by Nadia Marina Gaspar Baptista on 30.08.24.
+//
+
+import SwiftUI
+
+struct RitualsCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RitualsCardView()
+}
